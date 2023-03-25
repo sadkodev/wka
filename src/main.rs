9bd10b8313b541f7a06314edbadf204e38d57a6a
sadkodev <kristiankastillo0@gmail.com>
2023-03-25 05:12:29 -0400

    fix: thhiss

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "wka", author = "sadkodev <sadkodev@testmail.com>", version, about = "Does awesome things ", long_about = None)]
+struct Cli {
+    // Optional name to operate on
+    name: Option<String>,
+    // Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    configs: Option<PathBuf>,
+    // Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(short, long)]
+    list: Option<String>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// list test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    Down {}
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check the value provided by positional arguments, or option arguments.
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+        println!(" este es un test")
+
+    }
+
+    if let Some(_list) = cli.list.as_deref() {
+        println!("Si esto funciona aqui puedo poner la logica")
+    }
+
+    if let Some(config_path) = cli.configs.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...")
+            }
+        }
+        None => {}
+    }
+}
